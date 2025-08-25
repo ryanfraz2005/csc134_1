dabd9c53046ebd04c3c74b28432f36cb68850133
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-08-25 14:59:42 +0000

    added text

---
+++ m1t1.cpp
@@ -0,0 +1,22 @@
+/*
+m1t1 - hello, user
+Ryan F, 8/25/5
+
+INPUT: run the program
+PROCESS: None yet
+OUTPUT: return 0 (no error)
+SPECIAL FX; Greet the user.
+*/
+
+// magic words to start
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // SFX; Greet user
+    cout << "Hello, user!" << endl; << endl;
+
+    // output
+    return 0; // no errors
+}
