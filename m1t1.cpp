fc2a3cc06ad9b79310cd60852abaf54b5aaf2135
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-08-25 15:06:16 +0000

    M1T1, successfully greeted the user

--- m1t1.cpp
+++ m1t1.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main()
 {
     // SFX; Greet user
-    cout << "Hello, user!" << endl; << endl;
+    cout << "Hello, user!" << endl;
 
     // output
     return 0; // no errors
