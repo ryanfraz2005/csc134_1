57045b62226e61a21411d1dc7c4e2e65ffc0d1be
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-08-27 14:32:16 +0000

    begun coding coin sale program

---
+++ m1lab.cpp/m1lab.cpp
@@ -0,0 +1,22 @@
+/*
+csc 134
+m1lab
+Ryan Frazee
+8/27/2025
+purpose: The "coin" (replace noun) sales program.
+*/
+
+// magic words
+#include <iostream>
+using namespace std; 
+
+int main ()
+{
+    // start program
+    string item = "coin"; // replace with your item
+
+    cout << "Welcome to the " << item << " store!" << endl;
+
+    // end program
+    return 0;
+}
