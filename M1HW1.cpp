11f1a13045db19cbe442460b3fc718d43e97dad5
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-08-27 15:46:49 +0000

    Completed M1HW1

---
+++ M1HW1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+string movie = "Avengers: Endgame";
+int year = 2019;
+double box_office = 2.799;
+
+cout << "The movie " << movie << " released in " << year << ", and had a worldwide gross of around " << box_office << " billion dollars." << endl;
+
+cout << "Avengers: Endgame is the most excited I have ever been to see a movie." << endl;
+cout << "The \"Avengers Assemble\" scene remains my personal favorite of the movie." << endl;
+cout << "But my runner-up favorite scene is at the end of the final battle when Thanos said, \"I am inevitable.\" " << endl;
+cout << "To which Tony responded, \"... and I am Iron Man.\" " << endl;
+cout << "Of course an honorable mention to Steve lifing Mjolnir." << endl;
+
+return 0;
+}
