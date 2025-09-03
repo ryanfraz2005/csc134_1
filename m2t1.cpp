2345abdff95bc89140a97ee40fe26f20362bbda8
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-09-03 14:32:48 +0000

    started M2T1, just lab 1 with input

---
+++ m2t1.cpp
@@ -0,0 +1,16 @@
+/*
+CSC 134, M2T1
+Ryan Frazee
+9/3/25
+Revision of "sales" program from M1, now with user input.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
