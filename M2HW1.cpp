887edd8a9c37602c8b943e0410aa79e01058a069
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-09-15 14:40:31 +0000

    started m2hw1

---
+++ M2HW1.cpp
@@ -0,0 +1,29 @@
+/*
+CSC 134
+M2HW1 -
+Ryan Frazee
+9/15/25
+*/
+#include <iostream>
+using namespace std;
+
+void question1()
+{
+    cout << "Question 1" << endl;
+}
+void question2()
+{
+    cout << "Question 2" << endl;
+}
+
+int main ()
+{
+    // Call each question as its own function
+
+    question1();
+    question2();
+
+
+
+    return 0;
+}
