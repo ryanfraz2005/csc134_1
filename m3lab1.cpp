329e734715a02b4d261b83656d5820fb2a8ae572
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-09-24 14:27:09 +0000

    prototype of m3lab1

---
+++ m3lab1.cpp
@@ -0,0 +1,41 @@
+// if statement example2
+// standard header
+
+
+// compiler directives
+// (required for the program to run)
+#include <iostream>
+using namespace std;
+
+// beginning of the main() method
+int main() {
+  
+  // this program will ask a question and respond to it.
+  // You should run it, and test it by typing in different values.
+  // Example test values: 1, 2, 3, banana (try all of them)
+
+  // declare the variable we need
+  int choice; 
+
+  // ask the question
+  cout << "Do you choose Door 1 or Door 2?" << endl;
+  cout << "Type 1 or 2: "; 
+  cin >> choice;
+
+  // using if, make a decision based on the user's choice
+
+  if (1 == choice) {
+  	cout << "You chose Door 1" << endl;
+  }
+  else if (2 == choice) {
+  	cout << "You chose Door 2" << endl;
+  }
+  else {
+  	cout << "I'm sorry, that is not a valid choice." << endl;
+  }
+
+  // finish up
+  cout << "Thanks for playing!" << endl; // this runs no matter what they choose
+  return 0; // exit without error
+
+} // end of the main() method
