3902712780c564b2608248e58bc6e6e717bcf64b
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-09-29 14:33:23 +0000

    started m3lab2

---
+++ m3lab2.cpp
@@ -0,0 +1,30 @@
+/*
+M3LAB2 - Letter Grade (changed to Game Mechanics Test)
+CSC 134
+Ryan Frazee
+9/29/25
+*/
+
+#include <iostream>
+#include <cstdlib> // for rand() and srand()
+#include <ctime> // for time()
+
+using namespace std;
+
+void letter_grader();
+void combat();
+
+int main() 
+{
+    letter_grader();
+
+    return 0;
+}
+
+// DEFINE the other functions here
+
+void letter_grader()
+{
+    // input a number grade
+    // respond with a letter grade
+}
