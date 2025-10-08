c603aad1f63174c86d0a32fa28ecf344e081fa68
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-10-08 15:04:19 +0000

    finished m4lab2

---
+++ m4lab2.cpp
@@ -0,0 +1,34 @@
+/*
+CSC 124
+M4LAB2 - Nested Loops and Rectangles
+Ryan Frazee
+10/8/25
+
+Task: Draw a rectangle.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int length = 10, height = 10;
+    string tile = "🟦";
+
+    cout << "Draw a Rectangle" << endl;
+    cout << "Length? ";
+    cin >> length;
+    cout << "Height? ";
+    cin >> height;
+
+    cout << "Rectangle" << " = " << length << " x " << height << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < length; j++) {
+            cout << tile;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
