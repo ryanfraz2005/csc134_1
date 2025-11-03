f87dbc99f7be2dc46c2b340c73e8dc0e46301182
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-11-03 16:31:55 +0000

    started question 2 and added menu

--- m5hw1.cpp
+++ m5hw1.cpp
@@ -2,19 +2,45 @@
 #include <string>
 using namespace std;
 
+void menu();
 void question1();
+void question2();
 
 int main () {
 
-question1();
+menu();
 
 return 0;
+}
+
+void menu() {
+    cout << "MAIN MENU" << endl;
+    cout << "---------" << endl;
+    cout << "1 = Question 1" << endl;
+    cout << "2 = Question 2" << endl;
+    cout << endl;
+    cout << "Enter choice: ";
+    int choice;
+    cin >> choice;
+
+    if (choice == 1) {
+        question1();
+    }
+    else if (choice == 2) {
+        question2();
+    }
+    else {
+        cout << "Invalid choice." << endl << endl;
+        cin.clear();
+        menu();
+    }
 }
 
 void question1() {
 string month1, month2, month3;
 double rain1, rain2, rain3, average;
 
+cout << endl;
 cout << "Enter first month: ";
 cin >> month1;
 cout << "Enter rainfall for " << month1 << ": ";
@@ -30,4 +56,25 @@ cin >> rain3;
 average = (rain1 + rain2 + rain3) / 3;
 
 cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << average << endl;
+}
+
+void question2() {
+double length, width, height, volume;
+
+cout << endl;
+cout << "Finding the volume of a block." << endl;
+cout << "Enter Length: ";
+cin >> length;
+cout << "Enter Width: ";
+cin >> width;
+cout << "Enter Height: ";
+cin >> height;
+volume = length * width * height;
+
+if (length <= 0, width <= 0, height <= 0) {
+    cout << "Invalid Input." << endl;
+    cout << "Input cannot be less than 0." << endl;
+    question2();
+}
+
 }
