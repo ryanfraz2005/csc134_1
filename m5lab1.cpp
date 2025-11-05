fac8fdc3949f23c0014ffcfd08c008f43781986a
ryanfraz2005 <frazeer4253@student.faytechcc.edu>
2025-11-05 16:34:19 +0000

    worked on m5lab1, added choices

--- m5lab1.cpp
+++ m5lab1.cpp
@@ -5,8 +5,12 @@ int getPlayerChoice(int maxChoice);
 void showChoices(string choice1, string choice2, string choice3);
 void game_start();
 const int MAX = 3;
+void gameOver();
 
 int main() {
+
+    game_start();
+
     /*
     int choice;
     int max = 3;
@@ -33,6 +37,7 @@ int getPlayerChoice(int maxChoice) {
 }
 
 void showChoices(string choice1, string choice2, string choice3) {
+    cout << endl;
     cout << "---- MAKE YOUR CHOICE ----" << endl;
     int num = 1;
     cout << num << ". " << choice1 << endl;
@@ -51,12 +56,23 @@ void showChoices(string choice1, string choice2, string choice3) {
 void game_start() {
   int choice;
 // R for Raw -- Print every line as is until it ends
-  cout << R"()";
+  cout << R"(You wake up in a dark haunted house, confused of how you got there.)" << endl;
+  cout << R"(You hear footsteps walking down the hallway. What do you do?)";
 
-  showChoices(" ");
+  showChoices("Try and exit through the window.", "Hide under the bed.", "Hide in the closet.");
 
   getPlayerChoice(MAX);
   if (choice == 1) {
-    cout << " " << endl;
+    cout << R"(At first, the window won't budge. But eventually you successfully open it, but you find youself several stories up.)" << endl;
+    cout << R"(With no way out, you are caught.)" << endl;
+    gameOver();
   }
+  if (choice == 2) {
+    
+  }
+}
+
+void gameOver() {
+    cout << endl;
+    cout << " - - - -  G A M E   O V E R  - - - - " << endl;
 }
